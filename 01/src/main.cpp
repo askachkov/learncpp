91a29c0ed973ab87d13edff26a4c89a981994603
Oleksii Skachkov <a.skachkov.home@gmail.com>
2019-08-18 15:50:45 +0300

    01

--- 01/src/main.cpp
+++ 01/src/main.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <utility>
+#include <new>
 
 int x = 0;
 
@@ -22,6 +24,11 @@ class _Tracer
 			num = x++;
 			printTrace("CopyCtor");
 		}
+		_Tracer(const _Tracer&&)
+		{
+			num = x++;
+			printTrace("MoveCtor");
+		}
 		~_Tracer()
 		{
 			printTrace("Dtor");
@@ -31,15 +38,41 @@ class _Tracer
 			printTrace("OpEqual");
 			return *this;
 		}
+		_Tracer& operator=(const _Tracer&&)
+		{
+			printTrace("MoveEqual");
+			return *this;
+		}
+		
+		//https://en.cppreference.com/w/cpp/memory/new/operator_new
+		//https://en.cppreference.com/w/cpp/memory/new/operator_delete
+		
 		/*static*/ void * operator new(size_t s)
 		{
-			printf("Tracer: new\n");
+			printf("Tracer: new %d\n", s);
 			return calloc(s, 1);
 		}
 		/*static*/ void operator delete(void * p)
 		{
 			printf("Tracer: delete\n");
 			free(p);
+		}
+		
+		/*static*/ void * operator new[](size_t s)
+		{
+			printf("Tracer: new[] %d\n", s);
+			return calloc(s, 1);
+		}
+		/*static*/ void operator delete[](void * p)
+		{
+			printf("Tracer: delete[]\n");
+			free(p);
+		}
+		
+		/*static*/ void * operator new(size_t s, void *p)
+		{
+			printf("Tracer: placement new\n");
+			return p;
 		}
 };
 
@@ -57,10 +90,26 @@ int main(int argc, char * argv[])
 {
 	Tracer * pB;
 	{
+		Tracer * mem = (Tracer*)malloc(sizeof(Tracer));
+		
 		Tracer a;
-		pB = new Tracer(a);
+		new (mem) Tracer;
+		pB = mem;
+		
+		Tracer c = std::move(a);
+		Tracer d;
+		d = std::move(c);
 	}
+	printf("Tracer: remove object from 'placement new'\n");
+	pB->~Tracer();
+	free(pB);
+	
+	pB = new Tracer;
 	delete pB;
+	
+	pB = new Tracer[4];
+	delete[] pB;
+	
 	
 	{
 		Record r1;
